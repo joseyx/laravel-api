7e38ff2b0d3494cb0728f84652e4d525bcfe1d61
Jose Rojas <joseyx041199@gmail.com>
2023-08-19 04:03:04 -0400

    added get Empresa route

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -7,4 +7,6 @@ Route::post('/dashboard/form', [EmpresaController::class, 'store'])
                 ->middleware('auth:sanctum', 'isAdmin')
                 ->name('register_empresa');
 
-
+Route::get('/dashboard/empresa', [EmpresaController::class, 'index'])
+                ->middleware('auth:sanctum', 'isAdmin')
+                ->name('get_empresa');
